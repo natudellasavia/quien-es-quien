c79e420f05461467f0c2592eb912b0ed3a0d3303
natudellasavia <80315756+natudellasavia@users.noreply.github.com>
2025-03-03 22:26:15 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,73 @@
+// Get the buttons from the HTML
+let zero = document.getElementById("zero");
+let one = document.getElementById("one");
+let two = document.getElementById("two");
+let three = document.getElementById("three");
+let four = document.getElementById("four");
+let five = document.getElementById("five");
+let six = document.getElementById("six");
+let seven = document.getElementById("seven");
+let eight = document.getElementById("eight");
+let nine = document.getElementById("nine");
+let ten = document.getElementById("ten");
+let eleven = document.getElementById("eleven");
+let twelve = document.getElementById("twelve");
+let thirteen = document.getElementById("thirteen");
+let fourteen = document.getElementById("fourteen");
+
+function enableButtons() {
+  zero.disabled = false;
+  one.disabled = false;
+  two.disabled = false;
+  three.disabled = false;
+  four.disabled = false;
+  five.disabled = false;
+  six.disabled = false;
+  seven.disabled = false;
+  eight.disabled = false;
+  nine.disabled = false;
+  ten.disabled = false;
+  eleven.disabled = false;
+  twelve.disabled = false;
+  thirteen.disabled = false;
+  fourteen.disabled = false;
+}
+
+enableButtons();
+
+// --ACCIONES DE CLICK EN LA BOTONERA DEL JUEGO
+function clickCards(boton) {
+  position = parseInt(boton.getAttribute("pos"));
+
+  if (position == 0) {
+    zero.src = "/QUIEN-ES-QUIEN/img/black/blackAgus.jpg";
+  } else if (position == 1) {
+    one.src = "/QUIEN-ES-QUIEN/img/black/blackAldi.jpg";
+  } else if (position == 2) {
+    two.src = "/QUIEN-ES-QUIEN/img/black/blackBri.jpg";
+  } else if (position == 3) {
+    three.src = "/QUIEN-ES-QUIEN/img/black/blackCele.jpg";
+  } else if (position == 4) {
+    four.src = "/QUIEN-ES-QUIEN/img/black/blackCoppi.jpg";
+  } else if (position == 5) {
+    five.src = "/QUIEN-ES-QUIEN/img/black/blackCroque.jpg";
+  } else if (position == 6) {
+    six.src = "/QUIEN-ES-QUIEN/img/black/blackEmi.jpg";
+  } else if (position == 7) {
+    seven.src = "/QUIEN-ES-QUIEN/img/black/blackGuada.jpg";
+  } else if (position == 8) {
+    eight.src = "/QUIEN-ES-QUIEN/img/black/blackLuchi.jpg";
+  } else if (position == 9) {
+    nine.src = "/QUIEN-ES-QUIEN/img/black/blackLuna.jpg";
+  } else if (position == 10) {
+    ten.src = "/QUIEN-ES-QUIEN/img/black/blackMale.jpg";
+  } else if (position == 11) {
+    eleven.src = "/QUIEN-ES-QUIEN/img/black/blackMica.jpg";
+  } else if (position == 12) {
+    twelve.src = "/QUIEN-ES-QUIEN/img/black/blackMonti.jpg";
+  } else if (position == 13) {
+    thirteen.src = "/QUIEN-ES-QUIEN/img/black/blackNati.jpg";
+  } else if (position == 14) {
+    fourteen.src = "/QUIEN-ES-QUIEN/img/black/blackRosa.jpg";
+  }
+}
